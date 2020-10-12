65d81754d25d90dda8ed92d6c70dcf2495c5d274
Vuong Huynh <hoangvuong19991964@gmail.com>
2020-10-13 00:26:28 +0700

    Init commit for BE

---
+++ BE_HRM/BE_HRM/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BE_HRM.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
